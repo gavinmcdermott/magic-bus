bad0fc03713216a6e3bf5f577f972d4ba6bfb5d8
Reid Williams <rwilliams@ideo.com>
2016-07-19 21:51:33 -0700

    wip

--- light.js
+++ light.js
@@ -1,8 +1,25 @@
 import { exec } from 'child_process'
 
+
+// returns a promise
+let readSensor = () => {
+	let command = 'i2cget -y 1 0X39 0XAC w'
+	return new Promise((resolve, reject) => {
+		exec(command, (error, stdout, stderr) => {
+			if (error) {
+				reject(error)
+			} else {
+				let decimal = parseInt(stdout.toString(), 16)
+				resolve(decimal)
+			}
+		})	
+	})
+}
+
 // returns a promise
 let initSensor = () => {
-	let command = 'i2cget -y 1 0X39 0XAC w'
+	// let command = 'i2cget -y 1 0X39 0XAC w'
+	let command = 'i2cset -y 1 0X39 0X0 0X3'
 	return new Promise((resolve, reject) => {
 		exec(command, (error, stdout, stderr) => {
 			if (error) {
