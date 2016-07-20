cd2e17e5bdcccfc32f5e0f0572ac459f155f2171
Reid Williams <rwilliams@ideo.com>
2016-07-19 22:16:15 -0700

    wip

--- sound.js
+++ sound.js
@@ -1,9 +1,27 @@
-import BeagleBone from 'beaglebone-io'
+// IMPORTANT!!!
 
-let board = new BeagleBone();
- 
-board.on('ready', function () {
-  this.analogRead('A0', function (value) {
-    console.log(value);
-  });
-});
+// first run as root
+// echo cape-bone-iio > /sys/devices/bone_capemgr.*/slots
+
+
+
+import { exec } from 'child_process'
+
+
+// returns a promise
+let readSensor = () => {
+	let command = 'at /sys/devices/ocp.3/helper.15/AIN0'
+	return new Promise((resolve, reject) => {
+		exec(command, (error, stdout, stderr) => {
+			if (error) {
+				reject(error)
+			} else {
+				resolve(parseInit(stdout.toString()))
+			}
+		})	
+	})
+}
+
+setInterval(() => {
+	readSensor().then(console.log)
+}, 1000)
